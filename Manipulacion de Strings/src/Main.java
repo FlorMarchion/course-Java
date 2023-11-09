e6a2433b5bae58f60d189f9e4989ede86ef00bb5
Florencia Marchiondelli <fflor777@hotmail.com>
2023-11-09 20:29:04 -0300

    Strings Homework finished

--- Manipulacion de Strings/src/Main.java
+++ Manipulacion de Strings/src/Main.java
@@ -1,10 +1,8 @@
 import java.util.Scanner;
 
-
 public class Main {
     public static void main(String[] args) {
         int cantidadDePalabras;
-        int searchWord;
         String phrase = "";
         String word = "";
         do{
@@ -13,20 +11,28 @@ public class Main {
             phrase = scanner.nextLine();
             String[] palabras = phrase.split("\\s+");
             cantidadDePalabras = palabras.length;
-            System.out.println("Cantidad de palabras: " + cantidadDePalabras);
         }while (cantidadDePalabras <= 2);
         Phrases frase = new Phrases(phrase);
 
         //Cantidad de caracteres
       frase.numberOfCharacters();
 
-        // Contiene cierta palabra?
-
-        // Contiene cierta palabra?
+       // Contiene cierta palabra?
         Scanner scanner = new Scanner(System.in);
         System.out.println("¿Quieres saber si existe cierta palabra dentro de la frase? Ingresa la palabra a continuación:");
         word = scanner.nextLine();
-
         frase.containsWord(word);
+
+        //Upper & Lowe case
+        frase.lowercaseUppercase();
+
+        //Extraer primer palabra de la  frase
+        frase.printFirstWord();
+
+        //Agregar una palabra a la frase
+        System.out.println("Escribe algo más (precedido de un espacio) para agregar al final de tu frase: ");
+        word = scanner.nextLine();
+        System.out.println("Este es el resultado de tu frase:");
+        frase.addWord(word);
     }
 }
--- Manipulacion de Strings/src/Phrases.java
+++ Manipulacion de Strings/src/Phrases.java
@@ -1,7 +1,6 @@
 public class Phrases {
     String phrase;
     String word;
-
     public Phrases(String phrase) {
         this.phrase = phrase;
     }
@@ -14,27 +13,33 @@ public class Phrases {
     public void containsWord(String word){ //indexOf()
         if (word != null) {
             int palabra = phrase.indexOf(word);
-            System.out.println("palabra ingresada: " + palabra);
             if (palabra != -1) {
-                System.out.println("La palabra " + word + " existe dentro de la frase !");
+                System.out.println("La palabra *" + word + "* existe dentro de la frase !");
             } else {
-                System.out.println("No se encontró la palabra " + word + " dentro de la frase. :( ");
+                System.out.println("No se encontró la palabra *" + word + "* dentro de la frase. :( ");
             }
         }
         else {
         System.out.println("La palabra no ha sido ingresada.");
         }
     }
-
-
-
     public void lowercaseUppercase(){
-        System.out.println("Mayúsculas y minúsculas");
+        if(phrase.equals(phrase.toLowerCase())){
+            String phraseToUpperCase = phrase.toUpperCase();
+            System.out.println("Tu frase a sido convertida en mayúculas: " + phraseToUpperCase );
+        }else{
+            String phraseToLowerCase = phrase.toLowerCase();
+            System.out.println("Tu frase a sido convertida en minúsculas: " + phraseToLowerCase );
+        }
     }
 
-    public void addWord(){
-        System.out.println("Agragar palabras en cualquier parte de la frase");
+    public void printFirstWord(){
+        int space = phrase.indexOf(" ");
+        String firstWord = phrase.substring(0, space);
+        System.out.println("La primer palabra de tu frase es: " + firstWord);
     }
 
-
+    public void addWord(String word){
+        System.out.println(phrase.concat(word));
+    }
 }
