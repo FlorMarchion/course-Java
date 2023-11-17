0f69977002a48d22babda474819496ffffdc2f77
Florencia Marchiondelli <fflor777@hotmail.com>
2023-11-17 20:03:33 -0300

    Working with Collections

--- Collections/src/Main.java
+++ Collections/src/Main.java
@@ -1,13 +1,27 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Objects;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+        String respuesta = "no";
 
-        TaskManager.addTask();
+        do {
+            TaskManager.addTask();
+            System.out.println("Desea agregar otra tarea ? (Responder si/no): ");
+            respuesta = scanner.nextLine();
+        }while(Objects.equals(respuesta, "si"));
+
+        System.out.println("Desea eliminar una tarea? (Responder si/no): ");
+        respuesta = scanner.nextLine();
+        if(Objects.equals(respuesta, "si")){
+          TaskManager.removeTask();
+        }
+
+
+       // TaskManager.printTaskList();
         TaskManager.markTaskAsCompleted();
-        TaskManager.removeTask();
-        TaskManager.printTaskList();
+
 
     };
     };
--- Collections/src/TaskManager.java
+++ Collections/src/TaskManager.java
@@ -1,34 +1,80 @@
 import java.util.ArrayList;
     import java.util.List;
+import java.util.Scanner;
 
-    public class TaskManager {
+public class TaskManager {
     private static List<Tarea> listaDeTareas = new ArrayList<Tarea>();
     public static void addTask() {
-        listaDeTareas.add(new Tarea("Pasear a Bender", "Hoy", 5));
-        listaDeTareas.add(new Tarea("Ir al supermercado", "hoy", 9));
+    //Agrego la tarea nueva desde la consola:
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ingresa la descrición de la tarea: ");
+        String description = scanner.nextLine();
+
+        System.out.println("Ingrese la fecha en que debe realizar la tarea en formato dd/mm/yy: ");
+        String fecha = scanner.nextLine();
+
+        System.out.println("Puntuar del 1 al 10 la prioridad de la tarea, siendo 10 el número más alto: ");
+        int prioridad = scanner.nextInt();
+
+        //Agrego la tarea a la lista:
+        listaDeTareas.add(new Tarea(description, fecha, prioridad));
+
+        System.out.println("LA TAREA SE HA GUARDADO CORRECTAMENTE! :)");
 
     }
 
-    public static void markTaskAsCompleted(){
-    //Obtengo tareas específicas por índice:
-        Tarea tareasCompletadas = listaDeTareas.get(1);
-        tareasCompletadas.setCompleted(true);
-        System.out.println("La siguiente tarea ha sido completada: " + tareasCompletadas.getDescripcion());
+    public static void markTaskAsCompleted() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ingrese descripción de la tarea que has completado: ");
+        String word = scanner.nextLine();
+
+        for (int i = 0; i < listaDeTareas.size(); i++) {
+            Tarea tarea = listaDeTareas.get(i);
+
+            if (tarea.getDescripcion().contains(word)) {
+                System.out.println("Tarea que contiene la palabra " + word.toUpperCase() + ": " + tarea.getDescripcion());
+                Tarea tareasCompletadas = listaDeTareas.get(i);
+                tareasCompletadas.setCompleted(true);
+                System.out.println("La tarea ha sido completada :) !");
+            } else {
+                System.out.println("No se econtraron tareas con: " + word.toUpperCase());
+
+            }
+            if (tarea.isCompleted()) {
+                System.out.println("Catidad de tareas completadas: " + listaDeTareas.size());
+            } else {
+                System.out.println("Aún no ha completado ninguna tarea :(");
+            }
+        }
+    }
+    public  static void removeTask() {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Indique el número de la tarea que desea eliminar: ");
+
+        //lista de tareas con indice:
+        for (int i = 0; i < listaDeTareas.size(); i++) {
+            Tarea tarea = listaDeTareas.get(i);
+            System.out.println(i + "- " + tarea.getDescripcion());
+
+            int taskToDelete = scanner.nextInt();
+
+            Tarea tareaRemoved = listaDeTareas.remove(taskToDelete);
+            System.out.println("La siguiente tarea ha sido removida de la lista: " + tareaRemoved.getDescripcion());
+        }
     }
 
-    public  static void removeTask(){
-        Tarea tareaRemoved = listaDeTareas.remove(1);
-        System.out.println("La siguiente tarea ha sido removida de la lista: " + tareaRemoved.getDescripcion());
-    }
-
-    public static void printTaskList(){
+    /*public static void printTaskList(){
         //Mostrar lista de tareas pendientes:
         for(Tarea tareas : listaDeTareas){
-            System.out.println("Lista de tareas pendientes: " + tareas.getDescripcion());
+            System.out.println("Tarea pendiente: " + tareas.getDescripcion());
         }
         //Total de tareas a completar:
        int totalDeTareas =  listaDeTareas.size();
         System.out.println("Total de tareas a completar: " + totalDeTareas);
-    }
+    }*/
 
     };
